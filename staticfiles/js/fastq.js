aab23ad8a01d4ce56e74f07fc7ab3adbcbe4030e
Erdemozer01 <ozer246@gmail.com>
2026-06-15 16:48:08 +0300

    staticfiles git takibinden cikarildi (.gitignore zaten vardi)

--- staticfiles/js/fastq.js
+++
@@ -1,7 +0,0 @@
-// JavaScript konfigürasyonu (template veya static file'da)
-Resumable({
-    target: '/bio-tools/fastq-analyzer/API/resumable',
-    chunkSize: 5 * 1024 * 1024,  // 5 MB
-    simultaneousUploads: 3,
-    testChunks: false
-});
