ad1ce8ae685288bad333655cf933a0456e5bd8cb
Erdemozer01 <ozer246@gmail.com>
2026-06-17 20:52:55 +0300

    güncellendi

--- blog/migrations/0005_generatedarticle_last_edited_at.py
+++ blog/migrations/0005_generatedarticle_last_edited_at.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0004_reference_check_fields'),
+        ('blog', '0004_generatedarticle_reference_check_result_and_more'),
     ]
 
     operations = [
